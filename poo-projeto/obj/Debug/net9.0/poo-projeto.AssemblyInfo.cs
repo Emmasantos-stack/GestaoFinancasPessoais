ffb7c60a6468db35b2b4ab016ab67c09529c8f50
Emmasantos-stack <emanuelatfsantos@gmail.com>
2025-12-22 17:20:22 +0000

    .

--- poo-projeto/obj/Debug/net9.0/poo-projeto.AssemblyInfo.cs
+++ poo-projeto/obj/Debug/net9.0/poo-projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poo-projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6ea1177c245cda0a05549ddd2eee3273e80e2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c153d8ed0a7a3c19969b785437b5d80abafb1344")]
 [assembly: System.Reflection.AssemblyProductAttribute("poo-projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poo-projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
